0e3b73e8522959ca46c34aea2b3dcde6f79b802c
stefano.perelli <sedprc@gmail.com>
2019-09-03 19:01:38 +0200

    Change fork

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# Sutro — a <a href='https://github.com/kyleamathews/typography.js'>Typography.js</a> theme
+# Otis — a <a href='https://github.com/kyleamathews/typography.js'>Typography.js</a> theme
 
-A theme for Typography.js
+A Typography.js theme optimized for readability
 
 ## Install
 ```bash
-npm install --save typography typography-theme-sutro
+npm install --save typography typography-theme-otis
 ```
 ## Usage
 ```javascript
 import Typography from 'typography'
-import sutroTheme from 'typography-theme-sutro'
+import sutroTheme from 'typography-theme-otis'
 
-const typography = new Typography(sutroTheme)
+const typography = new Typography(otisTheme)
 ```
 ## More on theme
 
-Sutro uses "<a href='https://fonts.google.com/specimen/Open+Sans'>Open Sans</a>" for headers and "<a href='https://fonts.google.com/specimen/Merriweather'>Merriweather</a>" for body text
+Otis uses "<a href='https://fonts.google.com/specimen/Fira+Sans'>Fira Sans</a>" for headers and "<a href='https://fonts.google.com/specimen/Rubik'>Rubik</a>" for body text
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import gray from 'gray-percentage';
-import type { OptionsType } from 'Types';
-import { MOBILE_MEDIA_QUERY, TABLET_MEDIA_QUERY } from 'typography-breakpoint-constants';
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants';
 
-const theme: OptionsType = {
+const theme = {
 	title: 'Otis',
 	baseFontSize: '18px',
 	baseLineHeight: 1.55,
@@ -24,24 +23,36 @@ const theme: OptionsType = {
 	bodyWeight: 300,
 	boldWeight: 700,
 	overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+		h1: {
+			fontSize: rhythm(1.5),
+			marginTop: rhythm(1),
+			marginBottom: rhythm(1)
+		},
+		h3: {
+			fontSize: rhythm(0.75)
+		},
+		'h2,h3,h4,h5,h6': {
+			marginTop: rhythm(2)
+		},
 		a: {
-			color: '#8b0bd6',
+			color: '#aa04dc',
 			textDecoration: 'none'
 		},
 		'a:hover,a:active': {
-			textDecoration: 'underline'
+			opacity: 0.5,
+			transition: 'opacity .15s ease-in-out'
 		},
-		p: {
-			marginBottom: rhythm(0.8),
-			marginTop: rhythm(0.8)
+		time: {
+			color: '#aa04dc',
+			fontWeight: 400
 		},
 		blockquote: {
 			...scale(1 / 5),
 			color: gray(41),
 			fontStyle: 'italic',
 			paddingLeft: rhythm(13 / 16),
-			marginLeft: 0,
-			borderLeft: `${rhythm(3 / 16)} solid ${gray(80)}`
+			marginLeft: rhythm(-1),
+			borderLeft: `${rhythm(3 / 16)} solid ${gray(10)}`
 		},
 		'blockquote > :last-child': {
 			marginBottom: 0
@@ -70,20 +81,25 @@ const theme: OptionsType = {
 				paddingLeft: rhythm(9 / 16)
 			}
 		},
-		[TABLET_MEDIA_QUERY]: {
-			h1: {
-				...scale(5 / 5)
-			}
+		'mark,ins': {
+			background: '#007acc',
+			color: 'white',
+			padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
+			textDecoration: 'none'
 		},
-		'h1,h2,h3,h4,h5,h6': {
-			marginTop: rhythm(2)
+		code: {
+			backgroundColor: '#f4f6f9',
+			padding: '2px 5px',
+			borderRadius: '2px'
 		},
-		h1: {
-			...scale(6 / 5),
-			letterSpacing: '1px'
+		'.gatsby-highlight > .language-text': {
+			backgroundColor: '#f4f6f9',
+			padding: '2px 5px',
+			borderRadius: '2px'
 		},
-		h6: {
-			fontStyle: 'italic'
+		'.gatsby-highlight code': {
+			background: 'none',
+			padding: 0
 		}
 	})
 };
